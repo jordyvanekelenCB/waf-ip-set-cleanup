e5e90e1a972217677f66bc9077543737d781e669
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-11-12 15:05:54 +0100

    Fixed app for lint.

--- LambdaCode/app.py
+++ LambdaCode/app.py
@@ -1,34 +1,42 @@
+""" Entry point file """
+
+import os
 import logging
 import configparser
-import os
 from http_flood_clean import HTTPFloodClean
 
 # Setup logger
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 # Setup config parser
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), 'config', 'config.ini'))
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.join(os.path.dirname(__file__), 'config', 'config.ini'))
 
+
+# pylint: disable=W0613
 def lambda_handler(event, context):
+    """ Entry point of the Lambda function """
 
     # Activate HTTP flood clean
-    http_clean_results = HTTPFloodClean(config).clean_http_flood()
+    http_clean_results = HTTPFloodClean(CONFIG).clean_http_flood()
 
     # Print the results
     print_results(http_clean_results)
 
 
 def print_results(http_clean_results_obj):
+    """ Prints results to screen """
 
     block_list_queue_expired = http_clean_results_obj['block_list_queue_expired']
     block_list_ip_set_expired = http_clean_results_obj['block_list_ip_set_expired']
 
-    logger.info('================================ Http flood clean results ================================')
+    LOGGER.info('================================ Http flood clean results ================================')
 
-    for ip in block_list_ip_set_expired:
-        logger.info("Finding: Removed client ip: " + ip + " from IP Set")
+    for ip_address in block_list_ip_set_expired:
+        # pylint: disable=W1202
+        LOGGER.info("Finding: Removed client ip: {0} from IP Set".format(ip_address))
 
-    for ip in block_list_queue_expired:
-        logger.info("Finding: Removed client ip: " + ip + " from queue")
+    for ip_address in block_list_queue_expired:
+        # pylint: disable=W1202
+        LOGGER.info("Finding: Removed client ip: {0} from queue".format(ip_address))
